df3466d3a69fbb158449d2652c3874d4f9266252
fabricio <maluco13>
2020-04-05 21:51:32 -0300

    custom swagger ui

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,8 @@ namespace sessions_api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+
             app.UseSwagger();
 
             app.UseSwaggerUI(c =>
